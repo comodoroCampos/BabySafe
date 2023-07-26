d056cafd66fa66cd57da474e433772fd35e4a310
comodoroCampos <30672223+comodoroCampos@users.noreply.github.com>
2023-07-25 21:46:32 -0400

    first commit

---
+++ src/main/java/com/programa/BabySafeApplication.java
@@ -0,0 +1,13 @@
+package com.programa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabySafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabySafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/programa/BabySafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabySafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
